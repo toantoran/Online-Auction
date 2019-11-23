2409152d61dd7a78ab458f7fe1158aca1225962d
Phan Tan Thinh <thinh.phantan.75@gmail.com>
2019-11-24 02:30:56 +0700

    Fix

--- Sources/js/main.js
+++ Sources/js/main.js
@@ -1,11 +1,11 @@
-(function($) {
+(function ($) {
   "use strict"
 
-  $("#btn-call-signup").on('click', function() {  
+  $("#btn-call-signup").on('click', function () {
     $('#btn-exit-signin').trigger('click');
     $('#link-signup').trigger('click');
   });
-  $("#btn-call-signin").on('click', function() {  
+  $("#btn-call-signin").on('click', function () {
     $('#btn-exit-signup').trigger('click');
     $('#link-signin').trigger('click');
   });
@@ -13,12 +13,12 @@
   $('.cat-list>li>a').addClass('cat-link').attr('cat-order', index => index);
 
   $('.cat-link').hover((event) => {
-      changeCatImage(event);
+    changeCatImage(event);
   });
-  
+
   function changeCatImage(event) {
-      let img = $(event.target).closest('.row').find('.cat-img');
-      img.attr('src', 'img/' + $(event.target).attr('cat-order') + '.jpg')
+    let img = $(event.target).closest('.row').find('.cat-img');
+    img.attr('src', 'img/' + $(event.target).attr('cat-order') + '.jpg')
   }
 
 
@@ -26,7 +26,7 @@
     window.location.href = $(event.target).parent().parent().find('.product-body .product-name a').attr('href');
   });
   $('.btn-wish').click((event) => {
-    let target = $(event.target).closest('.watch-list.btn-wish')
+    let target = $(event.target).closest('.btn-wish')
     target.toggleClass('focus');
     if (target.hasClass('focus')) {
       new SnackBar({
@@ -45,7 +45,7 @@
       });
     }
   });
-  
+
 
   // NAVIGATION
   var responsiveNav = $('#responsive-nav'),
@@ -54,17 +54,17 @@
     menuToggle = $('#responsive-nav .menu-nav .menu-header'),
     menuList = $('#responsive-nav .menu-nav .menu-list');
 
-  catToggle.on('click', function() {
+  catToggle.on('click', function () {
     menuList.removeClass('open');
     catList.toggleClass('open');
   });
 
-  menuToggle.on('click', function() {
+  menuToggle.on('click', function () {
     catList.removeClass('open');
     menuList.toggleClass('open');
   });
 
-  $(document).click(function(event) {
+  $(document).click(function (event) {
     if (!$(event.target).closest(responsiveNav).length) {
       if (responsiveNav.hasClass('open')) {
         responsiveNav.removeClass('open');
@@ -98,21 +98,21 @@
     dots: false,
     arrows: true,
     responsive: [{
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          dots: false,
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
     ]
   }
 
@@ -126,21 +126,21 @@
     arrows: true,
     appendDots: '.product-slick-dots-latest',
     responsive: [{
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          dots: false,
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
     ]
   }
   $('#product-slick-1').slick(noSlick);
@@ -178,10 +178,10 @@
       connect: true,
       tooltips: [true, true],
       format: {
-        to: function(value) {
+        to: function (value) {
           return value.toFixed(0) + ' triệu';
         },
-        from: function(value) {
+        from: function (value) {
           return value
         }
       },
@@ -194,8 +194,8 @@
   }
 
   //Bid Table
-  
-  function Bid (time, name, price) {
+
+  function Bid(time, name, price) {
     this.time = time;
     this.name = name;
     this.price = price;
@@ -220,24 +220,160 @@
       new Bid("20/11/2019 19:20", "****Toàn", "6,300,000")
     ],
     columns: [
-      { data: 'time' },
-      { data: 'name' },
-      { data: 'price' },
+      { data: 'time', sTitle: 'Thời gian' },
+      { data: 'name', sTitle: 'Người đấu giá' },
+      { data: 'price', sTitle: 'Giá' }
     ],
-    'headers': ["Thời gian", "Người mua", 'Giá'],
     "language": {
       "emptyTable": "Chưa có lượt ra giá nào",
       "info": "",
       "infoEmpty": "",
       "lengthMenu": "Hiện _MENU_ lượt ra giá",
       "paginate": {
-        "first":      "Đầu",
-        "last":       "Cuối",
-        "next":       "Sau",
-        "previous":   "Trước"
+        "first": "Đầu",
+        "last": "Cuối",
+        "next": "Sau",
+        "previous": "Trước"
       }
     }
   });
+
+  $('tbody tr').append("<td><button class='main-btn refuse-btn'>Từ chối</button></td>");
+  $('.refuse-btn').click(() => {
+    Swal.fire({
+      title: 'Bạn có chắc chắn muốn từ chối lượt đấu giá này?',
+      text: "Bạn không thể hoàn tác",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#F8694A',
+      cancelButtonColor: '#000',
+      confirmButtonText: 'Chắn chắn',
+      cancelButtonText: 'Hủy bỏ'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Đã từ chối lượt đấu giá !',
+          'Người này không thể tham gia đấu giá sản phẩm này nữa',
+          'success'
+        )
+      }
+    })
+  })
+
+
+  var myMenu =
+    '<div>\
+    <a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>\
+    <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square fa-2x"></a>\
+    <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>\
+</div>';
+  $('#myPopUp').popup({
+    content: myMenu,
+    position: "bottom",
+    style: "orange",
+    animation: "standard",
+    event: "click",
+    hideOnClick: true,
+    zIndex: 100
+  });
+
+  $('.note-btn').click(() => {
+    $('.note-btn').prop('disabled', true);
+    var d = new Date();
+    var strDate = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+    $('.note-btn').parent().parent().parent().append(
+      '<div class="note">\
+        <p><i class="fas fa-edit"></i> <span class="note-date">' + strDate + '</span></p>\
+        <textarea></textarea>\
+      </div>\
+      <div class="deletable pull-right">\
+        <button class="main-btn" id="done-note-btn">Xong</button>\
+        <button class="main-btn" id="cancel-note-btn">Hủy bỏ</button>\
+      </div>');
+
+    $('#done-note-btn').click(() => {
+      if ($('.note textarea').val().trim().length == 0) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Hmm......',
+          text: 'Nội dung không được để trống'
+        })
+      }
+      else {
+        Swal.fire({
+          title: 'Thêm ghi chú này?',
+          text: "Bạn không thể hoàn tác",
+          icon: 'info',
+          showCancelButton: true,
+          confirmButtonColor: '#F8694A',
+          cancelButtonColor: '#000',
+          confirmButtonText: 'Thêm',
+          cancelButtonText: 'Hủy bỏ'
+        }).then((result) => {
+          if (result.value) {
+            Swal.fire({
+              title: 'Đã thêm ghi chú!',
+              icon: 'success'
+            })
+            $('.note textarea').replaceWith('<p>' + $('.note textarea').val() + '<p>');
+            $('.note-btn').prop('disabled', false);
+            $('.deletable').remove();
+          }
+          else {
+            $('#cancel-note-btn').trigger('click');
+          }
+        })
+      }
+    });
+
+    $('#cancel-note-btn').click(() => {
+      $('.note-btn').prop('disabled', false);
+      $('.deletable').remove();
+      $('.note:last-child').remove();
+    });
+
+  });
+
+
+  $('.btn-bid').click(() => {
+    let priceStr = $('.btn-bid').parent().find('input.price-input').val();
+    let price = parseInt(priceStr.slice(0, priceStr.length - 1).replace(/,/g, ''));
+    console.log(price);
+
+    if (price <= getCurrentPrice()) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Giá không hợp lệ!',
+        text: 'Giá đưa ra phải lớn hơn giá hiện tại'
+      })
+    }
+    else {
+      Swal.fire({
+        title: priceStr,
+        text: "Bạn có chắc chắn với mức giá này?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#F8694A',
+        cancelButtonColor: '#000',
+        confirmButtonText: 'Ra giá <i class="fas fa-gavel"></i>',
+        cancelButtonText: 'Hủy bỏ'
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire({
+            title: 'Ra giá thành công',
+            icon: 'success'
+          })
+        }
+        else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Đã hủy lượt ra giá'
+          })
+        }
+      })
+    }
+  });
+
 
   const autoNumericOptionsEuro = {
     digitGroupSeparator: ',',
@@ -253,10 +389,10 @@
   function getValidPrice(currentPrice, priceStep) {
     return currentPrice + priceStep;
   }
-  
+
   function getCurrentPrice() {
     let a = $('.product-price#current').text();
-    return parseInt(a.slice(0, a.length - 1).replace(/,/g,''));
+    return parseInt(a.slice(0, a.length - 1).replace(/,/g, ''));
   }
 
   $('.price-input').val(getValidPrice(getCurrentPrice(), 100000));
@@ -264,7 +400,7 @@
   new AutoNumeric('.price-input', autoNumericOptionsEuro);
 
 
- 
+
 })(jQuery);
 
 
--- Sources/js/product-page.js
+++ Sources/js/product-page.js
@@ -12,11 +12,11 @@ let productSingle =
                 <li><span>00 S</span></li>\
             </ul>\
             <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem chi tiết</button>\
-            <span class="main-btn watch-list btn-wish"><i class="fa fa-heart"></i></span>\
+            <button class="watch-list btn-wish"><i class="fa fa-heart"></i></button>\
             <img src="./img/product01.jpg" alt="">\
         </div>\
         <div class="product-body">\
-            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>\
+            <h2 class="product-name"><a href="product-page.html">Product Name Goes Here</a></h2>\
             <p class="product-text">Giá hiện tại: <span class="product-price">6,300,000₫</span></p>\
             <div class="product-footer">\
                 <small><span>90</span> lượt ra giá</small>\
@@ -32,22 +32,7 @@ for (let i=0;i<5;i++) {
    productContainer.append(productSingle);
 }
 
-var myMenu =  
-'<div>\
-    <a href="#"><i class="fab fa-facebook-square fa-2x"></i></a>\
-    <a href="#"><i class="fab fa-twitter-square fa-2x"></a>\
-    <a href="#"><i class="fab fa-google-plus-square fa-2x"></i></a>\
-    <a href="#"><i class="fab fa-youtube fa-2x"></i></a>\
-</div>';
-$('#myPopUp').popup({	
-    content: myMenu,	
-    position: "bottom",
-    style: "orange",	
-    animation: "standard",	
-    event: "click",		
-    hideOnClick: true,	
-    zIndex: 100 
-});
+
     
 
 
