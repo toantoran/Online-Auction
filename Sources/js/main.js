9c31755c6eb4df01ddf3e764c8a29ca9fbb74ab4
Tran Quoc Toan <tranquoctoan51199@gmail.com>
2019-11-24 19:36:57 +0700

    Big Update

--- Sources/js/main.js
+++ Sources/js/main.js
@@ -1,85 +1,15 @@
 (function ($) {
   "use strict"
 
-  tinymce.init({
-    selector: '#editor',
-    height: 400
-  });
+  //Super Important
 
+  //Swtich button (multiple)
   var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-
-  elems.forEach(function(html) {
-    var switchery = new Switchery(html,  { color: '#F8694A', secondaryColor: '#30323a', jackColor: '#fff', jackSecondaryColor: '#fff',  size: 'small' });
+  elems.forEach(function (html) {
+    var switchery = new Switchery(html, { color: '#F8694A', secondaryColor: '#30323a', jackColor: '#fff', jackSecondaryColor: '#fff', size: 'small' });
   });
 
-  $('#filer_input').filer({
-    addMore: true,
-    extensions: ["jpg", "png", "gif"],
-    showThumbs: true,
-    templates: {
-      box: '<ul class="jFiler-items-list jFiler-items-default"></ul>',
-      item: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title" title="{{fi-name}}">{{fi-name | limitTo:30}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status">{{fi-progressBar}}</span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
-      itemAppend: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title">{{fi-name | limitTo:35}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status"></span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
-      progressBar: '<div class="bar"></div>',
-      itemAppendToEnd: false,
-      removeConfirmation: true,
-      canvasImage: true,
-      _selectors: {
-          list: '.jFiler-items-list',
-          item: '.jFiler-item',
-          progressBar: '.bar',
-          remove: '.jFiler-item-trash-action'
-      }
-    },
-    afterShow: () => {$('.jFiler-items-default .jFiler-item-assets a').html('<i class="fas fa-trash-alt"></i>')}
-  });
-
-  $('.main-btn.btn-new').click(() => {
-    Swal.fire({
-      title: 'Tạo phiên đấu giá cho sản phẩm này ?',
-      icon: 'info',
-      showCancelButton: true,
-      confirmButtonColor: '#F8694A',
-      cancelButtonColor: '#000',
-      confirmButtonText: 'Đấu giá sản phẩm <i class="fas fa-gavel"></i>',
-      cancelButtonText: 'Hủy bỏ'
-    }).then((result) => {
-      if (result.value) {
-        Swal.fire({
-          title: 'Sản phẩm đã được đấu giá',
-          icon: 'success',
-          footer: '<a href="product-page.html">Xem sản phẩm?</a>'
-        })
-      }
-    })
-  });
-
-  $('.main-btn.icon-btn.btn-delete').click(() => {
-    Swal.fire({
-      title: 'Bạn chắc chắn muốn gỡ bỏ sản phẩm này?',
-      text: 'Bạn không thể hoàn tác',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#F8694A',
-      cancelButtonColor: '#000',
-      confirmButtonText: 'Chắc chắn <i class="fas fa-trash"></i>',
-      cancelButtonText: 'Hủy'
-    }).then((result) => {
-      if (result.value) {
-        Swal.fire({
-          title: 'Sản phẩm đã được gỡ bỏ',
-          icon: 'success',
-          footer: '<a href="product-page.html">Xem sản phẩm?</a>',
-          timer: 4000,
-          footer: 'Điều hướng về trang chủ'
-        })
-        setTimeout(() => {
-          window.location.href = 'index.html';
-        }, 4000)
-      }
-    })
-  })
-
+  //SignIn SignUp Form
   $("#btn-call-signup").on('click', function () {
     $('#btn-exit-signin').trigger('click');
     $('#link-signup').trigger('click');
@@ -89,62 +19,15 @@
     $('#link-signin').trigger('click');
   });
 
+  //Navbar link list
   $('.cat-list>li>a').addClass('cat-link').attr('cat-order', index => index);
-
   $('.cat-link').hover((event) => {
     changeCatImage(event);
   });
-
-  $('.btn-signout').click(() => {
-    Swal.fire({
-      title: 'Bạn muốn đăng xuất?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#F8694A',
-      cancelButtonColor: '#000',
-      confirmButtonText: 'Đăng xuất',
-      cancelButtonText: 'Hủy'
-    }).then((result) => {
-      if (result.value) {
-        Swal.fire({
-          title: 'Đăng xuất thành công',
-          icon: 'success'
-        });
-      }
-    })
-  });
-
-
   function changeCatImage(event) {
     let img = $(event.target).closest('.row').find('.cat-img');
     img.attr('src', 'img/' + $(event.target).attr('cat-order') + '.jpg')
   }
-
-
-  $('.quick-view').click((event) => {
-    window.location.href = $(event.target).parent().parent().find('.product-body .product-name a').attr('href');
-  });
-  $('.btn-wish').click((event) => {
-    let target = $(event.target).closest('.btn-wish')
-    target.toggleClass('focus');
-    if (target.hasClass('focus')) {
-      new SnackBar({
-        message: "Đã thêm vào danh sách yêu thích",
-        status: "success",
-        fixed: true,
-        timeout: 2000
-      });
-    }
-    else {
-      new SnackBar({
-        message: "Đã xóa khỏi danh sách yêu thích",
-        status: "warning",
-        fixed: true,
-        timeout: 2000
-      });
-    }
-  });
-
 
   // NAVIGATION
   var responsiveNav = $('#responsive-nav'),
@@ -269,234 +152,186 @@
   // PRODUCT ZOOM
   $('#product-main-view .product-view').zoom();
 
-  // PRICE SLIDER
-  var slider = document.getElementById('price-slider');
-  if (slider) {
-    noUiSlider.create(slider, {
-      start: [0, 50],
-      connect: true,
-      tooltips: [true, true],
-      format: {
-        to: function (value) {
-          return value.toFixed(0) + ' triệu';
-        },
-        from: function (value) {
-          return value
-        }
-      },
-      step: 0.5,
-      range: {
-        'min': 0,
-        'max': 50
-      }
-    });
-  }
-
-  //Bid Table
-
-  function Bid(time, name, price) {
-    this.time = time;
-    this.name = name;
-    this.price = price;
-    return this;
-  };
-  $('#bid-table').DataTable({
-    searching: false,
-    sort: false,
-    paging: true,
-    data: [
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000"),
-      new Bid("20/11/2019 19:20", "****Toàn", "6,300,000")
-    ],
-    columns: [
-      { data: 'time', sTitle: 'Thời gian' },
-      { data: 'name', sTitle: 'Người đấu giá' },
-      { data: 'price', sTitle: 'Giá' }
-    ],
-    "language": {
-      "emptyTable": "Chưa có lượt ra giá nào",
-      "info": "",
-      "infoEmpty": "",
-      "lengthMenu": "Hiện _MENU_ lượt ra giá",
-      "paginate": {
-        "first": "Đầu",
-        "last": "Cuối",
-        "next": "Sau",
-        "previous": "Trước"
-      }
-    }
-  });
-
-  $('tbody tr').append("<td><button class='main-btn refuse-btn'>Từ chối</button></td>");
-  $('.refuse-btn').click(() => {
+  //Sign Out
+  $('.btn-signout').click(() => {
     Swal.fire({
-      title: 'Bạn có chắc chắn muốn từ chối lượt đấu giá này?',
-      text: "Bạn không thể hoàn tác",
+      title: 'Bạn muốn đăng xuất?',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#F8694A',
       cancelButtonColor: '#000',
-      confirmButtonText: 'Chắn chắn',
+      confirmButtonText: 'Đăng xuất',
+      cancelButtonText: 'Hủy'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          title: 'Đăng xuất thành công',
+          icon: 'success'
+        });
+      }
+    })
+  });
+
+  //Thumbnail Button
+  $('.quick-view').click((event) => {
+    window.location.href = $(event.target).parent().parent().find('.product-body .product-name a').attr('href');
+  });
+  $('.btn-wish').click((event) => {
+    let target = $(event.target).closest('.btn-wish')
+    target.toggleClass('focus');
+    if (target.hasClass('focus')) {
+      new SnackBar({
+        message: "Đã thêm vào danh sách yêu thích",
+        status: "success",
+        fixed: true,
+        timeout: 2000
+      });
+    }
+    else {
+      new SnackBar({
+        message: "Đã xóa khỏi danh sách yêu thích",
+        status: "warning",
+        fixed: true,
+        timeout: 2000
+      });
+    }
+  });
+
+
+
+  //Other
+
+  tinymce.init({
+    selector: '#editor',
+    height: 400
+  });
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+
+  $('#filer_input').filer({
+    addMore: true,
+    extensions: ["jpg", "png", "gif"],
+    showThumbs: true,
+    templates: {
+      box: '<ul class="jFiler-items-list jFiler-items-default"></ul>',
+      item: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title" title="{{fi-name}}">{{fi-name | limitTo:30}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status">{{fi-progressBar}}</span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
+      itemAppend: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title">{{fi-name | limitTo:35}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status"></span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
+      progressBar: '<div class="bar"></div>',
+      itemAppendToEnd: false,
+      removeConfirmation: true,
+      canvasImage: true,
+      _selectors: {
+        list: '.jFiler-items-list',
+        item: '.jFiler-item',
+        progressBar: '.bar',
+        remove: '.jFiler-item-trash-action'
+      }
+    },
+    afterShow: () => { $('.jFiler-items-default .jFiler-item-assets a').html('<i class="fas fa-trash-alt"></i>') }
+  });
+
+  $('.main-btn.btn-new').click(() => {
+    Swal.fire({
+      title: 'Tạo phiên đấu giá cho sản phẩm này ?',
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#F8694A',
+      cancelButtonColor: '#000',
+      confirmButtonText: 'Đấu giá sản phẩm <i class="fas fa-gavel"></i>',
       cancelButtonText: 'Hủy bỏ'
     }).then((result) => {
       if (result.value) {
-        Swal.fire(
-          'Đã từ chối lượt đấu giá !',
-          'Người này không thể tham gia đấu giá sản phẩm này nữa',
-          'success'
-        )
+        Swal.fire({
+          title: 'Sản phẩm đã được đấu giá',
+          icon: 'success',
+          footer: '<a href="product-page.html">Xem sản phẩm?</a>'
+        })
       }
     })
-  })
-
-
-  var myMenu =
-    '<div>\
-    <a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a>\
-    <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter-square fa-2x"></a>\
-    <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube fa-2x"></i></a>\
-</div>';
-  $('#myPopUp').popup({
-    content: myMenu,
-    position: "bottom",
-    style: "orange",
-    animation: "standard",
-    event: "click",
-    hideOnClick: true,
-    zIndex: 100
   });
 
-  $('.note-btn').click(() => {
-    $('.note-btn').prop('disabled', true);
-    var d = new Date();
-    var strDate = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
-    $('.note-btn').parent().parent().parent().append(
-      '<div class="note">\
-        <p><i class="fas fa-edit"></i> <span class="note-date">' + strDate + '</span></p>\
-        <textarea></textarea>\
-      </div>\
-      <div class="deletable pull-right">\
-        <button class="main-btn" id="done-note-btn">Xong</button>\
-        <button class="main-btn" id="cancel-note-btn">Hủy bỏ</button>\
-      </div>');
 
-    $('#done-note-btn').click(() => {
-      if ($('.note textarea').val().trim().length == 0) {
+  $('.section-title .title small').click(() => {
+    Swal.fire({
+      title: 'Bán hàng trên Bidhub',
+      text: 'Xác nhận các thông tin bên dưới và bạn sẽ được xem xét để trở thành người bán trong vòng 7 ngày',
+      icon: 'info',
+      confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> Tuyệt!',
+    })
+  });
+
+  $('.form-group .btn-term').click(() => {
+    Swal.fire({
+      title: 'Điều khoản dịch vụ',
+      icon: 'info',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. At soluta et voluptate ipsam nisi minima sint accusantium doloribus explicabo, ad est assumenda iste libero beatae sunt. Magni tenetur ut aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quia laborum fugit consectetur iste, reprehenderit veritatis voluptate veniam harum provident aperiam quidem esse? Magnam fugiat quidem omnis ratione vero nemo.',
+      confirmButtonColor: '#F8694A',
+      confirmButtonText: 'Đã hiểu',
+    })
+  });
+
+  $('.form-group .btn-seller-regis').click(() => {
+    Swal.fire({
+      title: 'Bạn chắc chắn muốn gửi yêu cầu đăng ký này?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#F8694A',
+      cancelButtonColor: '#000',
+      confirmButtonText: 'Đăng ký',
+      cancelButtonText: 'Hủy'
+    }).then((result) => {
+      if (result.value) {
         Swal.fire({
-          icon: 'error',
-          title: 'Hmm......',
-          text: 'Nội dung không được để trống'
+          title: 'Đã gửi yêu cầu đăng ký',
+          text: 'Ban quản trị sẽ kiểm duyệt tài khoản này trước khi xác nhận trở thành người bán',
+          icon: 'success'
+        })
+      }
+    })
+
+    return false;
+  });
+
+  $('.form-group .btn-edit-info').click(() => {
+    Swal.fire({
+      title: 'Bạn chắc chắn muốn lưu những thay đổi này?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#F8694A',
+      cancelButtonColor: '#000',
+      confirmButtonText: 'Lưu',
+      cancelButtonText: 'Hủy'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          title: 'Thông tin đã được cập nhật',
+          icon: 'success'
         })
       }
       else {
         Swal.fire({
-          title: 'Thêm ghi chú này?',
-          text: "Bạn không thể hoàn tác",
-          icon: 'info',
-          showCancelButton: true,
-          confirmButtonColor: '#F8694A',
-          cancelButtonColor: '#000',
-          confirmButtonText: 'Thêm',
-          cancelButtonText: 'Hủy bỏ'
-        }).then((result) => {
-          if (result.value) {
-            Swal.fire({
-              title: 'Đã thêm ghi chú!',
-              icon: 'success'
-            })
-            $('.note textarea').replaceWith('<p>' + $('.note textarea').val() + '<p>');
-            $('.note-btn').prop('disabled', false);
-            $('.deletable').remove();
-          }
-          else {
-            $('#cancel-note-btn').trigger('click');
-          }
+          title: 'Các thay đổi chưa được cập nhật',
+          icon: 'error'
         })
       }
     });
 
-    $('#cancel-note-btn').click(() => {
-      $('.note-btn').prop('disabled', false);
-      $('.deletable').remove();
-      $('.note:last-child').remove();
-    });
-
+    return false;
   });
 
+  var radio = document.querySelector('.js-switch-radio');
+  var radioInit = new Switchery(radio, { color: '#F8694A', secondaryColor: '#F8694A', jackColor: 'white', jackSecondaryColor: 'white' });
 
-  $('.btn-bid').click(() => {
-    let priceStr = $('.btn-bid').parent().find('input.price-input').val();
-    let price = parseInt(priceStr.slice(0, priceStr.length - 1).replace(/,/g, ''));
-    console.log(price);
-
-    if (price <= getCurrentPrice()) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Giá không hợp lệ!',
-        text: 'Giá đưa ra phải lớn hơn giá hiện tại'
-      })
-    }
-    else {
-      Swal.fire({
-        title: priceStr,
-        text: "Bạn có chắc chắn với mức giá này?",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#F8694A',
-        cancelButtonColor: '#000',
-        confirmButtonText: 'Ra giá <i class="fas fa-gavel"></i>',
-        cancelButtonText: 'Hủy bỏ'
-      }).then((result) => {
-        if (result.value) {
-          Swal.fire({
-            title: 'Ra giá thành công',
-            icon: 'success'
-          })
-        }
-        else {
-          Swal.fire({
-            icon: 'error',
-            title: 'Đã hủy lượt ra giá'
-          })
-        }
-      })
-    }
-  });
-
-
-  const autoNumericOptionsEuro = {
-    digitGroupSeparator: ',',
-    currencySymbol: '₫',
-    decimalPlaces: 0,
-    currencySymbolPlacement: AutoNumeric.options.currencySymbolPlacement.suffix,
-    roundingMethod: AutoNumeric.options.roundingMethod.halfUpSymmetric
-  };
-
-  // Initialization
-
-
-  function getValidPrice(currentPrice, priceStep) {
-    return currentPrice + priceStep;
-  }
-
-  function getCurrentPrice() {
-    let a = $('.product-price#current').text();
-    return parseInt(a.slice(0, a.length - 1).replace(/,/g, ''));
-  }
-
-  $('.price-input').val(getValidPrice(getCurrentPrice(), 100000));
-
-  new AutoNumeric('.price-input', autoNumericOptionsEuro);
 
 
 
--- Sources/js/sample/account.js
+++ Sources/js/sample/account.js
@@ -1,5 +1,5 @@
-let productSingle =
-'<div class="col-md-5ths col-sm-4 col-xs-6">\
+let productSuccess =
+    '<div class="col-md-3 col-sm-4 col-xs-6">\
 <div class="product product-single">\
 <div class="product-thumb" >\
         <div class="product-label">\
@@ -11,7 +11,7 @@ let productSingle =
         <li><span>00 M</span></li>\
         <li><span>00 S</span></li>\
     </ul>\
-    <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem chi tiết</button>\
+    <button class="main-btn quick-view"><i class="fas fa-money-check-alt"></i> Thanh toán</button>\
     <button class="watch-list btn-wish"><i class="fa fa-heart"></i></button>\
     <img src="./img/product01.jpg" alt="">\
 </div>\
@@ -20,7 +20,7 @@ let productSingle =
     <p class="product-text">Giá hiện tại: <span class="product-price" style="color: green">5,900,000₫</span></p>\
     <p class="product-text">Giá của bạn: <span class="product-price" style="color: green">5,900,000₫</span></p>\
     <p class="product-text">Trạng thái: <span class="product-price" style="color: red">Đã kết thúc</span></p>\
-    <p class="product-text">Kết quả: <span class="product-price" style="color: green">Thành công</span></p>\
+    <p class="product-text">Kết quả: <span class="product-price" style="color: green">Thắng cuộc</span></p>\
     <div class="product-footer">\
         <small><span>90</span> lượt ra giá</small>\
         <small>Từ <span>20/11/2019</span></small>\
@@ -29,8 +29,8 @@ let productSingle =
 </div>\
 </div>';
 
-let productSingleFail =
-    '<div class="col-md-5ths col-sm-4 col-xs-6">\
+let productFail =
+    '<div class="col-md-3 col-sm-4 col-xs-6">\
 <div class="product product-single">\
 <div class="product-thumb" >\
         <div class="product-label">\
@@ -60,11 +60,56 @@ let productSingleFail =
 </div>\
 </div>';
 
-let productContainer = $('.section .container .product-container');
 
-for (let i = 0; i < 3; i++) {
-    productContainer.append(productSingle);
+let productWish =
+    '<div class="col-md-3 col-sm-4 col-xs-6">\
+    <div class="product product-single">\
+        <div class="product-thumb" >\
+            <div class="product-label">\
+                <span class="sale">Hot <i class="fab fa-hotjar"></i></span>\
+                <span>Mới</span>\
+            </div>\
+            <ul class="product-countdown">\
+                <li><span>00 H</span></li>\
+                <li><span>00 M</span></li>\
+                <li><span>00 S</span></li>\
+            </ul>\
+            <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Xem chi tiết</button>\
+            <button class="watch-list btn-wish"><i class="fa fa-heart"></i></button>\
+            <img src="./img/product01.jpg" alt="">\
+        </div>\
+        <div class="product-body">\
+            <h2 class="product-name"><a href="product-page.html">Product Name Goes Here</a></h2>\
+            <p class="product-text">Giá hiện tại: <span class="product-price">6,300,000₫</span></p>\
+            <div class="product-footer">\
+                <small><span>90</span> lượt ra giá</small>\
+                <small>Từ <span>20/11/2019</span></small>\
+            </div>\
+        </div>\
+        </div>\
+    </div>';
+
+
+
+let productHistory = $('.section .container .product-container#history');
+let productWishList = $('.section .container .product-container#wish-list');
+let productSoldList = $('.section .container .product-container#sold-product');
+
+for (let i = 0; i < 2; i++) {
+    productHistory.append(productSuccess);
 }
 for (let i = 0; i < 2; i++) {
-    productContainer.append(productSingleFail);
+    productHistory.append(productFail);
 }
+
+for (let i = 0; i < 6; i++) {
+    productWishList.append(productWish);
+}
+for (let i = 0; i < 4; i++) {
+    productSoldList.append(productWish);
+}
+
+$('.datepicker-here').datepicker({
+    dateFormat: 'dd/mm/yyyy',
+    startDate: new Date('1999-1-1')
+})
