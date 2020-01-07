dc93ee9bf9d18669f10a54b10914ee16049a0135
Tran Quoc Toan <tranquoctoan51199@gmail.com>
2020-01-07 07:16:04 +0700

    Fix Add subcate img preview

--- public/js/admin-add-subcate.js
+++ public/js/admin-add-subcate.js
@@ -1,27 +1,89 @@
-$('#subcate-img').filer({
-    limit: 1,
-    clipBoardPaste: true,
-    extensions: ["jpg"],
-    showThumbs: true,
-    templates: {
-        box: '<ul class="jFiler-items-list jFiler-items-default"></ul>',
-        item: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title" title="{{fi-name}}">{{fi-name | limitTo:30}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status">{{fi-progressBar}}</span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
-        itemAppend: '<li class="jFiler-item"><div class="jFiler-item-container"><div class="jFiler-item-inner"><div class="jFiler-item-icon pull-left">{{fi-icon}}</div><div class="jFiler-item-info pull-left"><div class="jFiler-item-title">{{fi-name | limitTo:35}}</div><div class="jFiler-item-others"><span>size: {{fi-size2}}</span><span>type: {{fi-extension}}</span><span class="jFiler-item-status"></span></div><div class="jFiler-item-assets"><ul class="list-inline"><li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li></ul></div></div></div></div></li>',
-        progressBar: '<div class="bar"></div>',
-        itemAppendToEnd: true,
-        removeConfirmation: false,
-        canvasImage: true,
-        _selectors: {
-            list: '.jFiler-items-list',
-            item: '.jFiler-item',
-            progressBar: '.bar',
-            remove: '.jFiler-item-trash-action'
-        }
-    },
-    afterShow: () => {
-        $('.jFiler-items-default .jFiler-item-assets a').html('<i class="fas fa-trash-alt"></i>')
-    }
+$("#subcate-img").filer({
+	limit: 1,
+	clipBoardPaste: true,
+	extensions: ["jpg"],
+	showThumbs: true,
+	theme: "default",
+	templates: {
+		box: '<ul class="jFiler-items-list jFiler-items-grid"></ul>',
+		item:
+			'<li class="jFiler-item">\
+					<div class="jFiler-item-container">\
+						<div class="jFiler-item-inner">\
+							<div class="jFiler-item-thumb">\
+								<div class="jFiler-item-status"></div>\
+								<div class="jFiler-item-thumb-overlay">\
+									<div class="jFiler-item-info">\
+										<div style="display:table-cell;vertical-align: middle;">\
+											<span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name}}</b></span>\
+											<span class="jFiler-item-others">{{fi-size2}}</span>\
+										</div>\
+									</div>\
+								</div>\
+								{{fi-image}}\
+							</div>\
+							<div class="jFiler-item-assets jFiler-row">\
+								<ul class="list-inline pull-left">\
+									<li>{{fi-progressBar}}</li>\
+								</ul>\
+								<ul class="list-inline pull-right">\
+									<li><a class="icon-jfi-trash jFiler-item-trash-action"><i class="fas fa-trash-alt"></i></a></li>\
+								</ul>\
+							</div>\
+						</div>\
+					</div>\
+				</li>',
+		itemAppend:
+			'<li class="jFiler-item">\
+						<div class="jFiler-item-container">\
+							<div class="jFiler-item-inner">\
+								<div class="jFiler-item-thumb">\
+									<div class="jFiler-item-status"></div>\
+									<div class="jFiler-item-thumb-overlay">\
+										<div class="jFiler-item-info">\
+											<div style="display:table-cell;vertical-align: middle;">\
+												<span class="jFiler-item-title"><b title="{{fi-name}}">{{fi-name}}</b></span>\
+												<span class="jFiler-item-others">{{fi-size2}}</span>\
+											</div>\
+										</div>\
+									</div>\
+									{{fi-image}}\
+								</div>\
+								<div class="jFiler-item-assets jFiler-row">\
+									<ul class="list-inline pull-left">\
+										<li><span class="jFiler-item-others">{{fi-icon}}</span></li>\
+									</ul>\
+									<ul class="list-inline pull-right">\
+										<li><a class="icon-jfi-trash jFiler-item-trash-action"></a></li>\
+									</ul>\
+								</div>\
+							</div>\
+						</div>\
+					</li>',
+		progressBar: '<div class="bar"></div>',
+		itemAppendToEnd: false,
+		canvasImage: true,
+		removeConfirmation: false,
+		_selectors: {
+			list: ".jFiler-items-list",
+			item: ".jFiler-item",
+			progressBar: ".bar",
+			remove: ".jFiler-item-trash-action"
+		}
+	},
+	addMore: false,
+	clipBoardPaste: true,
+	excludeName: null,
+	afterShow: () => {
+		$(".jFiler-items-default .jFiler-item-assets a").html(
+			'<i class="fas fa-trash-alt"></i>'
+		);
+	}
 });
 
-let cateID = (window.location.pathname.slice(-1));
-$('.list-links').children('.sub').eq(cateID - 1).children().addClass('link-active')
+let cateID = window.location.pathname.slice(-1);
+$(".list-links")
+	.children(".sub")
+	.eq(cateID - 1)
+	.children()
+	.addClass("link-active");
